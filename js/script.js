63615ca520a6b4c1830aaf72a6a42e00b011f85a
josecuervo89 <joseyedo@gmail.com>
2023-06-28 16:12:22 -0400

    agregando comandos en js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-let camisas = "talles "
+let nombre = prompt("ingrese tu nombre");
+let apellido = prompt("ingrese su apellido");
+let socio = prompt("ingrese su numero de socio");
+let nombrecompleto = nombre + " " + apellido +  " " + socio;
 
+console.log("bienvenid@", nombrecompleto);
+
+
+
+let camisas = "talles ";
+const tienda = "casla deportes";
 
